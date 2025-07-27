ccf371cbf2ac98197e241186cf7ab278e11fdc8f
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2025-07-27 18:27:03 +0300

    created api function

---
+++ mobile-app/api/chatApi.ts
@@ -0,0 +1,21 @@
+import { google } from "@ai-sdk/google";
+import { streamText, UIMessage, convertToModelMessages } from "ai";
+
+async function POST(req: Request) {
+  const { messages }: { messages: UIMessage[] } = await req.json();
+
+  const result = streamText({
+    model: google("models/gemini-2.0-flash-exp"),
+    messages: convertToModelMessages(messages),
+    system: "You are a friend.",
+  });
+
+  return result.toUIMessageStreamResponse({
+    headers: {
+      "Content-Type": "application/octet-stream",
+      "Content-Encoding": "none",
+    },
+  });
+}
+
+export { POST };
